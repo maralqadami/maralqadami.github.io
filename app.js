72879617e290b19a90465278cab9cf596f913834
maral <Maral.Qadami02@gmail.com>
2025-10-28 09:32:46 +0330

    add login and register functions to app.js & create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const login = (user) => {
+    if (user.name === 'amin') {
+        return  'you are loged in'
+    } else {
+        return 'wrong user'
+    }
+};
+
+const register = (user) => {
+    return {user, token};
+};
